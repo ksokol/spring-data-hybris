828d19252c18eced3f9f98aecffb504651ef3bfa
Kamill Sokol <ksokol@communicode.de>
2014-04-21 10:06:35 +0200

    introduced HybrisEntityMetadata class

---
+++ src/main/java/org/springframework/data/hybris/repository/core/HybrisEntityMetadata.java
@@ -0,0 +1,38 @@
+package org.springframework.data.hybris.repository.core;
+
+import de.hybris.platform.core.model.ItemModel;
+import org.springframework.data.repository.core.EntityMetadata;
+
+import java.lang.reflect.Field;
+
+/**
+ * @author Kamill Sokol
+ */
+public class HybrisEntityMetadata<T extends ItemModel> implements EntityMetadata<T> {
+
+    private final Class<T> domainType;
+    private final String typecode;
+
+    public HybrisEntityMetadata(Class<T> domainType) {
+        this.domainType = domainType;
+        this.typecode = extractTypecode(domainType);
+    }
+
+    @Override
+    public Class<T> getJavaType() {
+        return domainType;
+    }
+
+    public String getTypecode() {
+        return typecode;
+    }
+
+    private String extractTypecode(Class<?> domainType) {
+        try {
+            Field typecode = domainType.getDeclaredField("_TYPECODE");
+            return (String) typecode.get(null);
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
+}
---
+++ src/main/java/org/springframework/data/hybris/repository/core/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Hybris specific support classes for the Spring Data mapping subsystem.
+ */
+package org.springframework.data.hybris.repository.core;
+
